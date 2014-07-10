f850d13dee0aae1af4a6387f09610de51f6f21b3
Michel Hiemstra <mhiemstra@php.net>
2014-07-10 11:23:21 +0200

    custom

--- ganalytics.js
+++ ganalytics.js
@@ -1,12 +1,7 @@
 GAnalytics = {}
 
-GAnalytics.pageview = function(pageLocation) {
-    console.log("Analytics code is not loaded yet.");
-  };
-GAnalytics.event = function(category, action, label, value) {
-    console.log("Analytics code is not loaded yet.");
-  };
-
+GAnalytics.pageview = function(pageLocation) {};
+GAnalytics.event = function(category, action, label, value) {};
 
 load = function(i,s,o,g,r,a,m) {
   i['GoogleAnalyticsObject']=r;
@@ -19,34 +14,11 @@ load = function(i,s,o,g,r,a,m) {
     m.parentNode.insertBefore(a,m)
 };
 
-if(Meteor.settings && Meteor.settings.public !== undefined && Meteor.settings.public.ga !== undefined && Meteor.settings.public.ga.account !== undefined) {
+if(AppConfig.GANALYTICS) {
 
   load(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  var gaSettings = Meteor.settings.public.ga,
-      gaConfig = {};
-
-  // cookie settings
-  if(typeof gaSettings.cookieName !== 'undefined')
-    gaConfig.cookieName = gaSettings.cookieName;
-
-  if(typeof gaSettings.cookieDomain !== 'undefined')
-    gaConfig.cookieDomain = gaSettings.cookieDomain;
-  
-  if(typeof gaSettings.cookieExpires !== 'undefined')
-    gaConfig.cookieExpires = gaSettings.cookieExpires;
-
-  // if gaConfig is still empty, default it to 'auto'
-  if(Object.keys(gaConfig).length === 0)
-    gaConfig = 'auto';
-
-  ga('create', gaSettings.account, gaConfig);
-
-  if (gaSettings.trackInterests)
-    ga('require', 'displayfeatures');
-
-  if (gaSettings.trackInPage)
-    ga('require', 'linkid', 'linkid.js');
+  ga('create', AppConfig.GANALYTICS, 'auto');
 
   GAnalytics.pageview = function(pageLocation) {
     if(!pageLocation) {
@@ -54,13 +26,10 @@ if(Meteor.settings && Meteor.settings.public !== undefined && Meteor.settings.pu
     }
     ga('send', 'pageview', pageLocation);
   }
-  
+
   GAnalytics.event = function(category, action, label, value) {
     ga('send', 'event', category, action, label, value);
   }
 } else {
-  console.log("public.ga.account has not been set in your settings.json file.");
+  console.log("google analytics account has not been set");
 }
-
-
-
